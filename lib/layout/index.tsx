e0827332b6ce72d8f712596a72b60ba151888eed
itzhangjiayang <zhangjiayang@cs253.com>
2023-06-24 16:46:51 +0800

    update

--- lib/layout/index.tsx
+++ lib/layout/index.tsx
@@ -5,42 +5,51 @@ import Content from './content'
 import Footer from './footer'
 import Sider from './sider'
 import "./layout.scss"
-export default function index() {
+import "./layout.example.scss"
+export default function () {
   return (
     <div>
+      <div>
         <h1>第一个例子</h1>
-        <Layout>
-      <Header>Header</Header>
-      <Content>Content</Content>
-      <Footer>Footer</Footer>
-    </Layout>
-    <h1>第二个例子</h1>
-    <Layout>
-      <Header>Header</Header>
-      <Layout>
-        <Sider>Sider</Sider>
-        <Content>Content</Content>
-      </Layout>
-      <Footer>Footer</Footer>
-    </Layout>
-    <h1>第三个例子</h1>
-    <Layout>
-      <Header>Header</Header>
-      <Layout>
-        <Content>Content</Content>
-        <Sider>Sider</Sider>
-      </Layout>
-      <Footer>Footer</Footer>
-    </Layout>
-    <h1>第四个例子</h1>
-    <Layout>
-      <Sider>Sider</Sider>
-      <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
-    </Layout>
+        <Layout style={{height: 500, width: 500}} className="hi">
+          <Header className="x">header</Header>
+          <Content className="y">content</Content>
+          <Footer className="x">footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>第二个例子</h1>
+        <Layout style={{height: 500, width: 500}} className="hi">
+          <Header className="x">header</Header>
+          <Layout>
+            <Sider className="z">Sider</Sider>
+            <Content className="y">content</Content>
+          </Layout>
+          <Footer className="x">footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>第三个例子</h1>
+        <Layout style={{height: 500, width: 500}} className="hi">
+          <Header className="x">header</Header>
+          <Layout>
+            <Content className="y">content</Content>
+            <Sider className="z">Sider</Sider>
+          </Layout>
+          <Footer className="x">footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>第四个例子</h1>
+        <Layout style={{height: 500, width: 500}} className="hi">
+          <Sider className="z">Sider</Sider>
+          <Layout>
+            <Header className="x">header</Header>
+            <Content className="y">content</Content>
+            <Footer className="x">footer</Footer>
+          </Layout>
+        </Layout>
+      </div>
     </div>
-  )
+  );
 }
