d6be84f9a1de8de4f4517df23848f302eb5b171d
itzhangjiayang <zhangjiayang@cs253.com>
2023-06-24 16:33:20 +0800

    重构 classes

--- lib/classes.tsx
+++ lib/classes.tsx
@@ -7,27 +7,37 @@ interface ClassToggles {
   [K: string]: boolean
 }
 function scopedClassMaker(perfix: string) {
-  return function x(name?: string | ClassToggles, options?: options) {
-    let copyName;
-    let result;
-    if (typeof name === 'string' || name === undefined) {
-      copyName = name
-      result = [perfix, copyName].filter(Boolean).join('-')
-    } else {
-      copyName = Object.entries(name)
-        .filter((item) => item[1])
-        .map((item) => item[0])
-      result = copyName
-        .map((item) => {
-          return [perfix, item].filter(Boolean).join('-')
-        })
-        .join(' ')
-    }
-    if (options && options.extra) {
-      return [result, options.extra].filter(Boolean).join(' ')
-    } else {
-      return result
-    }
+  return function x(name: string | ClassToggles, options?: options) {
+    // let copyName;
+    // let result;
+    // if (typeof name === 'string' || name === undefined) {
+    //   copyName = name
+    //   result = [perfix, copyName].filter(Boolean).join('-')
+    // } else {
+    //   copyName = Object.entries(name)
+    //     .filter((item) => item[1])
+    //     .map((item) => item[0])
+    //   result = copyName
+    //     .map((item) => {
+    //       return [perfix, item].filter(Boolean).join('-')
+    //     })
+    //     .join(' ')
+    // }
+    // if (options && options.extra) {
+    //   return [result, options.extra].filter(Boolean).join(' ')
+    // } else {
+    //   return result
+    // }
+    //重构以后的
+   return Object
+    .entries(name instanceof Object ? name : {[name]: name})
+    .filter(kv => kv[1] !== false)
+    .map(kv => kv[0])
+    .map(name => [perfix, name]
+      .filter(Boolean)
+      .join('-'))
+    .concat(options && options.extra || [])
+    .join(' ');
   }
 }
 export default scopedClassMaker
